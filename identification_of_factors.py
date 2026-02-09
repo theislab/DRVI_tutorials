2ebf4555a408507d54b1d924ff545fe24fc9f83e
Amir Ali Moinfar <amirali.moinfar@helmholtz-muenchen.de>
2026-02-09 09:45:39 +0100

    Initial version of identification notebook

---
+++ identification_of_factors.py
@@ -0,0 +1,232 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.18.1
+#   kernelspec:
+#     display_name: python_apptainer
+#     language: python
+#     name: python_apptainer
+# ---
+
+# # Identification of factors
+
+# In this notebook, we use the already trained DRVI model on the immune dataset to show different ways to identify a factor. In summary we can:
+#
+# - Identify based on available annotation
+# - Identify using GSEA
+# - Identify using Language models
+# - Looking into the annotation databases
+#
+# **We always advise examination by a biologist or looking into the published litereture for validation of the identified processes.**
+
+# ## Intro
+#
+# In this notrbook, we assume that the user has already trained DRVI on Immune data.
+#
+# Please refer to [General training and interpretability pipeline](./general_pipeline.html) tutorial.
+
+# ## Contact
+
+# For questions and help requests, you can reach out in the [scverse discourse](https://discourse.scverse.org/).
+#
+# If you found a bug, please use the [issue tracker](https://github.com/theislab/drvi/issues).
+
+# ## Install
+
+# If you try DRVI on colab, next cell will install dependencies.
+#
+# Please remove this part if your environment is already setup.
+
+# +
+import sys
+
+# if branch is stable, will install via pypi, else will install from source
+branch = "latest"
+IN_COLAB = "google.colab" in sys.modules
+
+if IN_COLAB and branch == "stable":
+    # !pip install multigrate[tutorials]
+elif IN_COLAB and branch != "stable":
+    # !pip install git+https://github.com/theislab/drvi.git#egg=drvi[tutorials]
+# -
+
+# ## Imports
+
+import warnings
+warnings.filterwarnings("ignore")
+
+# +
+import anndata as ad
+import scanpy as sc
+
+import scvi
+import drvi
+from pathlib import Path
+from drvi.model import DRVI
+from drvi.utils.misc import hvg_batch
+# -
+
+print("Last run with scvi-tools version:", scvi.__version__)
+print("Last run with DRVI version:", drvi.__version__)
+
+# +
+# Making plots prettier
+sc.settings.set_figure_params(dpi=100, frameon=False)
+sc.set_figure_params(dpi=100)
+sc.set_figure_params(figsize=(3, 3))
+
+from matplotlib import pyplot as plt
+plt.rcParams["figure.dpi"] = 100
+plt.rcParams["figure.figsize"] = (3, 3)
+# -
+
+# ## Config
+
+# +
+# Set this to false if you already trained your model and do not like to retrain.
+overwrite = True
+SEED = 1  # Set to None if you don't want to set seed
+
+# Set input output directory
+# We use tmp_io/ directory in the same place as this notebook. Update accordingly.
+io_dir = Path("./tmp_io/drvi_immune_128/")
+io_dir.mkdir(parents=True, exist_ok=True)
+io_dir
+# -
+
+# ## Load Data
+
+# We already saved pre-processed data in previous notebook
+adata = sc.read_h5ad(io_dir / "adata_preprocesses.h5ad")
+
+# ## Load DRVI ouputs
+
+# +
+model_path = io_dir / "drvi_model"
+embed_path = io_dir / "embed.h5ad"
+traverse_adata_path = io_dir / "traverse_adata.h5ad"
+
+model = DRVI.load(model_path, adata)
+embed = sc.read_h5ad(embed_path)
+traverse_adata = sc.read_h5ad(traverse_adata_path)
+# -
+
+# ## Identify based on available annotations
+
+# In this dataset we have annotations stored in `adata.obs["final_annotation"]`.
+#
+# We first measure Scaled Mutual Information (SMI) between each latent dimension and each category using DRVI built-in functions.
+
+annot_col = "final_annotation"
+
+# ### Specific Imports
+
+import math
+import networkx as nx
+from drvi.utils.metrics import DiscreteDisentanglementBenchmark
+
+
+# ### Helper functions
+
+def plot_packed_network(df, title_col='title', var_col='variable', val_col='value'):
+    """
+    Visualizes network with edge weights shown to 2 decimal places.
+    """
+    # Create Graph
+    G = nx.from_pandas_edgelist(df, title_col, var_col, edge_attr=val_col)
+    
+    # Custom Grid Layout Logic
+    pos = {}
+    components = sorted(nx.connected_components(G), key=len, reverse=True)
+    cols = math.ceil(len(components)**0.5)
+    
+    for i, nodes in enumerate(components):
+        sub_pos = nx.spring_layout(G.subgraph(nodes), weight=val_col, k=0.5, seed=42)
+        r, c = divmod(i, cols)
+        for n, (x, y) in sub_pos.items():
+            pos[n] = (x + c * 3, y - r * 3)
+
+    plt.figure(figsize=(14, 10))
+    titles = set(df[title_col])
+    
+    # Draw Nodes & Edges
+    nx.draw(G, pos, 
+            with_labels=True, font_size=8, font_weight='bold', node_size=600,
+            node_color=['#A0CBE2' if n in titles else '#FF9E9E' for n in G.nodes()],
+            width=[d[val_col] * 4 for u, v, d in G.edges(data=True)], 
+            edge_color='grey', alpha=0.6)
+    
+    # Draw Edge Labels (Weights rounded to 2 decimals)
+    edge_labels = {(u, v): f"{d[val_col]:.2f}" for u, v, d in G.edges(data=True)}
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=8)
+            
+    plt.axis('off')
+    plt.show()
+
+
+# ### Code
+
+# Remove vanished dimensions
+embed_nv = embed[:, embed.var['vanished'] == False].copy()
+embed_nv
+
+benchmark = DiscreteDisentanglementBenchmark(
+    embed_nv.X, dim_titles=embed_nv.var['title'], discrete_target=embed.obs[annot_col],
+    metrics=["SMI-disc", "SPN"], aggregation_methods=["LMS"],
+)
+benchmark.evaluate()
+# You can optionally save benchmark object if you want.
+# benchmark.save(filename)
+# benchmark = DiscreteDisentanglementBenchmark.load(filename, embed_nv.X, dim_titles=embed_nv.var['title'], discrete_target=embed.obs["final_annotation"], metrics=["SMI-disc", "SPN"], aggregation_methods=["LMS"])
+
+smi_similarity = benchmark.get_results_details()["SMI-disc"]
+smi_similarity[:5]  # only showing 5 rows
+
+# +
+filtering_threshold = 0.5
+
+top_matches = (
+    smi_similarity.reset_index()
+    .melt(id_vars='title', value_vars=smi_similarity.columns)
+    .query("value >= @filtering_threshold")
+    .reset_index(drop=True)
+)
+top_matches
+# -
+
+plot_packed_network(top_matches)
+
+
+
+# #### Heatmap
+
+drvi.utils.pl.plot_latent_dims_in_heatmap(embed, "final_annotation", title_col="title")
+
+# It is possible to sort dimensions based on the top relevance with respect to a categoricals variable
+
+drvi.utils.pl.plot_latent_dims_in_heatmap(embed, "final_annotation", title_col="title", sort_by_categorical=True)
+
+
+
+
+# ## Identification of programs
+
+# Once we identify the top relevant genes, we can determine some programs through supervised external information, such as:
+# - existing annotations
+# - examination by biologists
+# - gene-set enrichment analysis (GSEA)
+# - scientific literature
+# - automated tools based on language models
+#
+# **Please refer to this tutorial for some tools that we found useful for identification of programs**
+#
+# It is worth mentioning that since such supervised information is not given to the model, the quality of the derived signatures is neither affected nor biased by it. Unidentified processes with high gene scores are promising candidates for further literature search, additional analysis, and even experimental design.
+
+
+
+
