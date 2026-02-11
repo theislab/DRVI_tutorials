c20c053c25d000d47331ef69c17b27b06887b9fa
Amir Ali Moinfar <amirali.moinfar@helmholtz-muenchen.de>
2026-02-11 13:10:06 +0100

    Add query to reference mapping notebook

---
+++ query_to_reference_mapping.py
@@ -0,0 +1,285 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.18.1
+#   kernelspec:
+#     display_name: python_apptainer
+#     language: python
+#     name: python_apptainer
+# ---
+
+# # Transfer learning to new dataset (query to reference mapping)
+
+# In this notebook, we train a DRVI model on reference data and transfer processes and embedings to a query data. DRVI uses acArches approach internally. This covers:
+#
+# - Training DRVI
+# - Query to reference mapping
+# - Observe the integrated latent space in UMAP
+# - Observe transferred factors
+
+# ## Contact
+
+# For questions and help requests, you can reach out in the [scverse discourse](https://discourse.scverse.org/).
+#
+# If you found a bug, please use the [issue tracker](https://github.com/theislab/drvi/issues).
+
+# ## Install
+
+# If you try DRVI on colab, next cell will install dependencies.
+#
+# Please remove this part if your environment is already setup.
+
+# +
+import sys
+
+# if branch is stable, will install via pypi, else will install from source
+branch = "latest"
+IN_COLAB = "google.colab" in sys.modules
+
+if IN_COLAB and branch == "stable":
+    # !pip install multigrate[tutorials]
+elif IN_COLAB and branch != "stable":
+    # !pip install git+https://github.com/theislab/drvi.git#egg=drvi[tutorials]
+# -
+
+# ## Imports
+
+import warnings
+warnings.filterwarnings("ignore")
+
+# +
+import anndata as ad
+import scanpy as sc
+
+import scvi
+import drvi
+from pathlib import Path
+from drvi.model import DRVI
+from drvi.utils.misc import hvg_batch
+# -
+
+print("Last run with scvi-tools version:", scvi.__version__)
+print("Last run with DRVI version:", drvi.__version__)
+
+# +
+# Making plots prettier
+sc.settings.set_figure_params(dpi=100, frameon=False)
+sc.set_figure_params(dpi=100)
+sc.set_figure_params(figsize=(3, 3))
+
+from matplotlib import pyplot as plt
+plt.rcParams["figure.dpi"] = 100
+plt.rcParams["figure.figsize"] = (3, 3)
+# -
+
+# ## Config
+
+# +
+# Set this to false if you already trained your model and do not like to retrain.
+overwrite = False
+SEED = 1  # Set to None if you don't want to set seed
+
+# Set input output directory to load data from and store model and embeddings there
+# We use tmp_io/ directory in the same place as this notebook. Update accordingly.
+io_dir = Path("./tmp_io/drvi_immune_128_q2r/")
+io_dir.mkdir(parents=True, exist_ok=True)
+io_dir
+# -
+
+# ## Load Data
+
+# + magic_args="-s \"$io_dir\"" language="bash"
+# export io_dir=$1
+#
+# # Download Example Immune dataset if it does not exist
+# if [ ! -f $io_dir/immune_all.h5ad ]; then
+#   curl -L https://figshare.com/ndownloader/files/25717328 -o $io_dir/immune_all.h5ad
+#   echo "File downloaded successfully."
+# else
+#   echo "File already exists."
+# fi
+# -
+
+adata = sc.read_h5ad(io_dir / "immune_all.h5ad")
+# Remove dataset with non-count values
+adata = adata[adata.obs["batch"] != "Villani"].copy()
+# We shuffle the data for better visualization. Otherwise order of points in UMAP will not be random.
+sc.pp.subsample(adata, fraction=1.)
+adata
+
+# ## Pre-processing
+
+adata.X = adata.layers["counts"].copy()
+sc.pp.normalize_total(adata)
+sc.pp.log1p(adata)
+adata
+
+adata.obs['study'].unique()
+
+# Holding out an unseed dataset as query
+adata_ref = adata[adata.obs['study'] != 'Sun'].copy()
+adata_query = adata[adata.obs['study'] == 'Sun'].copy()
+
+# Batch aware HVG selection (method is obtained from scIB metrics)
+hvg_genes = hvg_batch(adata_ref, batch_key="batch", target_genes=2000, adataOut=False)
+adata_ref = adata_ref[:, hvg_genes].copy()
+adata_query = adata_query[:, hvg_genes].copy()
+adata_ref, adata_query
+
+
+# Save pre-processes data for next notebooks
+if overwrite or not (io_dir / "adata_preprocesses_ref.h5ad").exists():
+    adata_ref.write_h5ad(io_dir / "adata_preprocesses_ref.h5ad")
+if overwrite or not (io_dir / "adata_preprocesses_query.h5ad").exists():
+    adata_query.write_h5ad(io_dir / "adata_preprocesses_query.h5ad")
+
+del adata
+
+# ## Train DRVI
+
+# +
+# You can also skip this cell if model is already trained
+
+# Setup data
+DRVI.setup_anndata(
+    adata_ref,
+    layer="counts",
+    batch_key="batch",
+    # In addition to batch_key, you can also provide additional `categorical_covariate_keys`.
+    # DRVI supports query to reference mapping with new categorical covatiates.
+    is_count_data=True,
+)
+
+# Setting seed (set to None if you don't want to fix seed)
+scvi.settings.seed = SEED
+
+# construct the model
+model = DRVI(
+    adata_ref,
+    n_latent=128,
+    # IMPORTANT: you need to allow encoder to get covatiates as input to be able to do query to reference mapping
+    encode_covariates=True,
+    # For encoder and decoder dims, provide a list of integers.
+    encoder_dims=[128, 128],
+    decoder_dims=[128, 128],
+    # dispersion='gene-batch',  # depending on the variability of gene dispersions use 'gene' or 'gene-batch'
+)
+model
+
+# +
+n_epochs = 400
+model_path = io_dir / "drvi_model"
+
+# train the model and save (if not already trained)
+if overwrite or not model_path.exists():
+    model.train(
+        max_epochs=n_epochs,
+        early_stopping=False,
+        early_stopping_patience=20,
+        # No need to provide `plan_kwargs` if n_epochs >= 400.
+        plan_kwargs={
+            "n_epochs_kl_warmup": n_epochs,
+        },
+    )
+    
+    # Save the model
+    model.save(model_path, overwrite=True)
+# -
+
+
+
+# ## Get reference embeddings
+
+# Load the model
+model = DRVI.load(model_path, adata_ref)
+model
+
+embed_ref = ad.AnnData(model.get_latent_representation(adata_ref), obs=adata_ref.obs)
+drvi.utils.tl.set_latent_dimension_stats(model, embed_ref, vanished_threshold=0.1)
+embed_ref.write_h5ad(io_dir / "embed_ref.h5ad")
+
+# If you are interested to observe your latent space and latent factors of the reference model, please have a look at the main tutorial.
+
+
+
+# ## Transfer learning
+
+# +
+model_path = io_dir / "drvi_model_transfer"
+if overwrite or not model_path.exists():
+    drvi.model.DRVI.prepare_query_anndata(adata_query, model)
+    model_transfer = drvi.model.DRVI.load_query_data(adata_query, model)
+    
+    # It is important to pass plan_kwargs={"weight_decay": 0.0} to make sure reference embeddings are untouched
+    model_transfer.train(
+        max_epochs=100, 
+        plan_kwargs={
+            "weight_decay": 0.0,
+        }
+    )
+    model_transfer.save(model_path, overwrite=True)
+
+model_transfer = DRVI.load(model_path, adata_query)
+# -
+
+
+
+# ## Latent space
+
+# +
+embed_path = io_dir / "embed.h5ad"
+
+# Create latent space data in anndata format
+if overwrite or not embed_path.exists():
+    embed_ref = sc.read_h5ad(io_dir / "embed_ref.h5ad")  # Reference
+    embed_query = ad.AnnData(model_transfer.get_latent_representation(), obs=adata_query.obs)  # Query
+
+    # Combining the two
+    embed_ref.obs['split'] = 'reference'
+    embed_query.obs['split'] = 'query'
+    embed = ad.concat([embed_ref, embed_query], join='outer')
+    embed.var = embed_ref.var.copy()
+
+    sc.pp.neighbors(embed, n_neighbors=10, use_rep="X", n_pcs=embed.X.shape[1])
+    sc.tl.umap(embed, spread=1.0, min_dist=0.5, random_state=123)
+    sc.pp.pca(embed)
+    
+    embed.write_h5ad(embed_path)
+embed
+# -
+
+embed = sc.read_h5ad(embed_path)
+
+sc.pl.umap(embed, color=["batch", "split", "final_annotation"], ncols=1, frameon=False)
+
+
+sc.pl.umap(embed, mask_obs=embed.obs['split']=='query', color=["final_annotation"], ncols=1, frameon=False)
+
+# ### Plot latent dimensions
+
+# By default, vanished dimensions are not plotted. Change arguments if you would like to.
+
+# #### UMAP of factors for all cells
+
+drvi.utils.pl.plot_latent_dims_in_umap(embed)
+
+# #### UMAP of factors for query cells
+
+drvi.utils.pl.plot_latent_dims_in_umap(
+    embed,
+    mask_obs=embed.obs['split']=='query',
+)
+
+# #### Heatmaps
+
+drvi.utils.pl.plot_latent_dims_in_heatmap(embed_ref, "final_annotation", title_col="title")
+
+embed_query.var = embed_ref.var.copy()
+drvi.utils.pl.plot_latent_dims_in_heatmap(embed_query, "final_annotation", title_col="title")
+
+
